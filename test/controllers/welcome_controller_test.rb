0765e5ec89a70f99fde682259f204d1ab39099f9
Carlos Fernandez <carlosfernandez@MacBook-Pro-de-Carlos.local>
2018-04-09 12:56:25 +0200

    crear controlador Welcome index y contact

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get welcome_index_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get welcome_contact_url
+    assert_response :success
+  end
+
+end
