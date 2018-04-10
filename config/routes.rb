3335290d726d98bbeeb6e42d21667852c4f80962
Carlos Fernandez <carlosfernandez@MacBook-Pro-de-Carlos.local>
2018-04-10 10:19:41 +0200

    Bootstrap installer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :budgets
-  get 'welcome/index'
 
-  get 'welcome/contact'
+  root 'welcome#index'
+
+  get 'contacto'=>'welcome#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
