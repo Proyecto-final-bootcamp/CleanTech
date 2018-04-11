8833671cde9ed4f6156ace906cc5e177ec35883f
wimel <angel.contreras.mejias@gmail.com>
2018-04-11 19:26:31 +0200

    mas validaciones

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,3 +1,7 @@
 class Budget < ApplicationRecord
   validates :name, :address, presence: true
+  validates :phone, presence: true
+  validates :phone, length: { is: 9 }
+  validates :phone, numericality: { only_integer: true }
+  validates :observations, length: { minimum: 2 }
 end
