f109a41b82d979c981082e57d29d3955e37b3384
wimel <angel.contreras.mejias@gmail.com>
2018-04-12 18:06:04 +0200

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Config gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:     'smtp.gmail.com',
+    port:        587,
+    domain:      'gmail.com',
+    user_name:   "#{ENV['gmail_un']}@gmail.com",
+    password:     "#{ENV['gmail_pw']}",
+    # usa la gema Figaro para ocultar tus UN & PW!
+    authentication:  'plain',
+    enable_starttls_auto: true
+  }
+
 end
